fc50650f889609a168f60677b25eb28b5f4153aa
Joshua Lanham <1709478@park.edu>
2026-02-14 18:31:55 -0600

    temp class added

--- src/Temperature.java
+++ src/Temperature.java
@@ -1,3 +1,25 @@
 public class Temperature {
-    
+    private final String city;
+    private final String state;
+    private final int low;
+    private final int high;
+
+    public Temperature(String city, String state, int low, int high) {
+        this.city = city;
+        this.state = state;
+        this.low = low;
+        this.high = high;
+    }
+     public String getCity() { return city; }
+     public String getState() {return state; }
+     public int getLow() { return low; }
+     public int getHigh() { return high; }
+
+     public int differential() {
+        return Math.abs(high - low);
+     }
+     @Override
+     public String toString() {
+        return city + ", " + state + " low=" + low + " high=" + high + " diff=" + differential();
+     }  
 }
